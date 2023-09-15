687f6fc73b4914697ab0e3f9478368671aa4980a
chrollos web dev <106138109+chro11os@users.noreply.github.com>
2023-09-15 10:39:51 +0800

    Java
    Java!

---
+++ codes/java/Training.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+public class Training {
+public static void main(String[] args){
+    System.out.println("Hello World");
+    }
+}
---
+++ codes/python/DataSci.py
@@ -0,0 +1,5 @@
+x = "Hello"
+if x == "Hello":
+    print ("Hello")
+else:
+    print ("Wtf")
